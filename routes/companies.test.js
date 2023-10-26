9f71dad5464eefbfc11b2bd5a05d55e3a1a4c71b
Matt Fergoda <mattfergoda@users.noreply.github.com>
2023-10-25 17:45:31 -0700

    Adding more tests with valid and invalid inputs.

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -120,19 +120,204 @@ describe("GET /companies", function () {
     });
   });
 
+  test("ok for anon: with only nameLike", async function () {
+    const resp = await request(app)
+      .get('/companies')
+      .query(
+        {
+          nameLike: '2',
+        });
+
+    expect(resp.body).toEqual({
+      companies:
+      [
+        {
+          handle: "c2",
+          name: "C2",
+          description: "Desc2",
+          numEmployees: 2,
+          logoUrl: "http://c2.img",
+        }
+      ],
+    });
+  });
+
+  test("ok for anon: with only minEmployees", async function () {
+    const resp = await request(app)
+      .get('/companies')
+      .query(
+        {
+          minEmployees: 2,
+        });
+
+    expect(resp.body).toEqual({
+      companies:
+        [
+          {
+            handle: "c2",
+            name: "C2",
+            description: "Desc2",
+            numEmployees: 2,
+            logoUrl: "http://c2.img",
+          },
+          {
+            handle: "c3",
+            name: "C3",
+            description: "Desc3",
+            numEmployees: 3,
+            logoUrl: "http://c3.img",
+          }
+        ],
+    });
+  });
+
+  test("ok for anon: with only maxEmployees", async function () {
+    const resp = await request(app)
+      .get('/companies')
+      .query(
+        {
+          maxEmployees: 2,
+        });
+
+    expect(resp.body).toEqual({
+      companies:
+        [
+          {
+            handle: "c1",
+            name: "C1",
+            description: "Desc1",
+            numEmployees: 1,
+            logoUrl: "http://c1.img",
+          },
+          {
+            handle: "c2",
+            name: "C2",
+            description: "Desc2",
+            numEmployees: 2,
+            logoUrl: "http://c2.img",
+          }
+        ],
+    });
+  });
+
+  test("ok for anon: with nameLike and minEmployees", async function () {
+    const resp = await request(app)
+      .get('/companies')
+      .query(
+        {
+          nameLike: '2',
+          minEmployees: 2,
+        });
+
+    expect(resp.body).toEqual({
+      companies:
+        [
+          {
+            handle: "c2",
+            name: "C2",
+            description: "Desc2",
+            numEmployees: 2,
+            logoUrl: "http://c2.img",
+          }
+        ],
+    });
+  });
+
+  test("ok for anon: with minEmployees and maxEmployees", async function () {
+    const resp = await request(app)
+      .get('/companies')
+      .query(
+        {
+          minEmployees: 2,
+          maxEmployees: 3,
+        });
+
+    expect(resp.body).toEqual({
+      companies:
+        [
+          {
+            handle: "c2",
+            name: "C2",
+            description: "Desc2",
+            numEmployees: 2,
+            logoUrl: "http://c2.img",
+          },
+          {
+            handle: "c3",
+            name: "C3",
+            description: "Desc3",
+            numEmployees: 3,
+            logoUrl: "http://c3.img",
+          }
+        ],
+    });
+  });
+
+  test("ok for anon: with nameLike and maxEmployees", async function () {
+    const resp = await request(app)
+      .get('/companies')
+      .query(
+        {
+          nameLike: '3',
+          maxEmployees: 3,
+        });
+
+    expect(resp.body).toEqual({
+      companies:
+        [
+          {
+            handle: "c3",
+            name: "C3",
+            description: "Desc3",
+            numEmployees: 3,
+            logoUrl: "http://c3.img",
+          }
+        ],
+    });
+  });
+
   test("return 400 error for invalid filter", async function () {
     const resp = await request(app)
       .get("/companies")
-      .query({monkey: 3000});
+      .query({ monkey: 3000 });
     expect(resp.body).toEqual({
       "error": {
-        "message": "monkey is not a valid filter.",
+        "message": [
+          "instance is not allowed to have the additional property \"monkey\"",
+        ],
         "status": 400
       }
     });
   });
 
-// TODO: more details on test
+  test("return 400 error for invalid minEmployees", async function () {
+    const resp = await request(app)
+      .get("/companies")
+      .query({ minEmployees: "foo" });
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance.minEmployees is not of a type(s) integer",
+        ],
+        "status": 400
+      }
+    });
+  });
+
+  test("return 400 error for invalid maxEmployees", async function () {
+    const resp = await request(app)
+      .get("/companies")
+      .query({ maxEmployees: "foo" });
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance.maxEmployees is not of a type(s) integer",
+        ],
+        "status": 400
+      }
+    });
+  });
+
 });
 
 /************************************** GET /companies/:handle */
